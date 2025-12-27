1a4eabfccf2c16ce7e76ff61e42e2a9cad13797c
Anjlesh Wasule <154040674+Anjlesh-Wasule@users.noreply.github.com>
2025-12-27 17:23:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	const fName = document.querySelector("input[name='fname']");
+	const lName = document.querySelector("input[name='lname']");
 
+	alert(`${fName.value} ${lName.value}`);
 }
